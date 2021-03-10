44232774143638457b59d1d3f1d3258719a04f88
joyce-git-cber <80428290+joyce-git-cber@users.noreply.github.com>
2021-03-11 07:00:27 +0800

    init hello world csharp

---
+++ hello-world csharp/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: acer
+ * Date: 3/11/2021
+ * Time: 6:57 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace hello_world_csharp
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
